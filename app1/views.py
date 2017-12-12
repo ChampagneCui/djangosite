6191a9c7f7a07e9421b1e2b0b4ea30a9f9bb2c61
ChampagneCui <xbcui2010@hotmail.com>
2017-12-12 09:22:41 +0800

    start

--- app1/views.py
+++ app1/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
+list=[{'hostname':'HostA','ip':'1.1.1.1','description':'test_server'}]
+
 
 def home(request):
-    return render(request,'home.html')
+    if request.method == "POST":
+        hostname=request.POST.get('hostname')
+        ip=request.POST.get('ip')
+        description=request.POST.get('des')
+        temp={'hostname':hostname,'ip':ip,'description':description}
+        list.append(temp)
+    return render(request,'home.html',{'host_list':list})
 
 def login(request):
     error_msg=''
@@ -12,7 +20,7 @@ def login(request):
         user = request.POST.get('user',None)
         pwd  = request.POST.get('passwd',None)
         if user == 'root' and pwd == '123':
-            return render(request,'home.html')
+            return redirect('/app1')
         else:
             #用户密码不匹配
             error_msg='用户名或密码错误'
